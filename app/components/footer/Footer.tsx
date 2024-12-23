0e8281e271f7edd40a32698d2f343d8b73c57c87
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2024-12-23 03:17:31 +0000

    functional footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,6 +1,36 @@
+"use client";
+import { useEffect } from "react";
 import Image from "next/image";
+import animejs from 'animejs';
+import useScrollDirection from "../../hooks/useScrollDirection";
 
-export const Footer = ()=>{return (
-<footer className="footer">
-    <Image src={'/logo.png'} width={70} height={70} alt="hooman hajarian"/>
-</footer>)};
+export const Footer = () => {
+
+    const scrollDirection = useScrollDirection({
+        initialDirection: "down",
+        thresholdPixels: 10,
+    });
+
+    useEffect(() => {
+        if (scrollDirection === "down") {
+            animejs({
+                targets: ".footer",
+                translateY: "-100%",
+                easing: "easeInOutSine",
+                duration: 1000,
+            });
+
+        } else {
+            animejs({
+                targets: ".footer",
+                translateY: "0%",
+                easing: "easeInOutSine",
+                duration: 1000,
+            });
+        }
+    }, [scrollDirection]);
+    return (
+        <footer className="footer">
+            <Image src={'/logo.png'} width={70} height={70} alt="hooman hajarian" />
+        </footer>)
+};
