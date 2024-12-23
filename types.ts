49854f5c0d40616b4e7a0180e07fa8acbfe3958e
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2024-12-23 03:16:10 +0000

    added types file

---
+++ types.ts
@@ -0,0 +1,12 @@
+// 2D Data Types
+export type ProjectDataType = {
+    id: number;
+    name: string;
+    position: string;
+    year: string;
+    href: string;
+    description: string;
+    imgUrl: string;
+    gitHub: string;
+    position3D: number[];
+};
