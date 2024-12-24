6ff891cc56bc6ca0a2e80a51abcba8e0a20e4533
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2024-12-24 01:22:10 +0000

    footer working animation

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -5,30 +5,31 @@ import animejs from 'animejs';
 import useScrollDirection from "../../hooks/useScrollDirection";
 
 export const Footer = () => {
-
     const scrollDirection = useScrollDirection({
         initialDirection: "down",
         thresholdPixels: 10,
     });
 
-    useEffect(() => {
-        if (scrollDirection === "down") {
-            animejs({
-                targets: ".footer",
-                translateY: "-100%",
-                easing: "easeInOutSine",
-                duration: 1000,
-            });
 
-        } else {
+    useEffect(() => {
+        if (scrollDirection === "up") {
             animejs({
                 targets: ".footer",
                 translateY: "0%",
                 easing: "easeInOutSine",
                 duration: 1000,
             });
+        } else {
+            animejs({
+                targets: ".footer",
+                translateY: "-100%",
+                easing: "easeInOutSine",
+                duration: 1000,
+            });
         }
     }, [scrollDirection]);
+
+
     return (
         <footer className="footer">
             <Image src={'/logo.png'} width={70} height={70} alt="hooman hajarian" />
